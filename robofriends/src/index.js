2b8175a5e47c3ab5f23eee52535aa3e375de4477
Justin Estaris <JustinEstaris@users.noreply.github.com>
2022-07-21 19:05:20 -0400

    Added Robot Cards

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'; // View library (Core Package)
+import ReactDOM from 'react-dom/client';  // Used to access the browser DOM.
 import './index.css';
-import App from './App';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons'; // Import the tachyons package.
+import {robots} from './robots';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root')); // Get the Root Element ID in index.html
+
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+    </div>
   </React.StrictMode>
 );
 
